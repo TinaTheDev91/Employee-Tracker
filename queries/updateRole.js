5d77ee9762d5fe29f1a9d1f468d798586ee9f3fb
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-04-18 20:04:53 -0400

    debugged updatedRole fn

--- queries/updateRole.js
+++ queries/updateRole.js
@@ -1,15 +1,17 @@
 const inquirer = require('inquirer');
 const connection = require('../config/connection');
 const cTable = require('console.table');
+const viewEe = require('./viewEe');
 
 function updateRole() {
-    connection.query(
-        `SELECT id, first_name, last_name, role_id from employee;`,
-        function (data) {
-            console.table(data)
-        }
-    ) 
-    inquirer.prompt([
+    connection.promise().query('SELECT * from employee')
+    .then(([data]) => {
+        let employees = data;
+        console.table(employees);
+        // console.log(`this is inside the updatedRole`);
+    })
+    
+    .then((employees) => inquirer.prompt([
         {
             type: 'input',
             message: 'Which employee would you like to update? Please enter their id.',
@@ -21,7 +23,7 @@ function updateRole() {
             message: 'What is the id of the new role?',
             name: 'role'
         }
-    ])
+    ]))
     .then(res => {
         connection.query(
             `UPDATE employee SET role_id = ${res.role} WHERE id = ${res.id};`,
